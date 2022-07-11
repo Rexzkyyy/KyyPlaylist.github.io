c30a08c631767140c830f3566bd88210ec68c2a5
Rexzkyyy <ikhsanloris@gmail.com>
2022-07-11 12:10:07 +0800

    Partamax

---
+++ js/music-list.js
@@ -0,0 +1,77 @@
+
+
+let allMusic = [
+  {
+    name: "Iwan Fals - Yang Terlupakan ",
+    artist: "Iwan Fals",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "GAYLE – abcdefu",
+    artist: "GAYLE",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Olivia Rodrigo - traitor ",
+    artist: "Olivia Rodrigo",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Rex Orange County - Pluto Projector",
+    artist: "Rex Orange County",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "NOAH - Yang Terdalam ",
+    artist: "NOAH",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Danilla - Senja Diambang Pilu",
+    artist: "Danilla",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Chrisye - Pergilah Kasih",
+    artist: "Chrisye",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "NIKI - Every Summertime",
+    artist: "NIKI",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "The Walters - I Love You So",
+    artist: "The Walters",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Stephen Sanchez - Until I Found You",
+    artist: " Stephen Sanchez",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: " IV of Spades - Come inside of my heart",
+    artist: " IV of Spades",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: " rex orange county - the shad",
+    artist: " rex orange county",
+    img: "music-12",
+    src: "music-12"
+  },
+ 
+];
